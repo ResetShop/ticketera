96642aa7b75ae7247669a536fd8b7a5840a2b577
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-08-06 08:21:04 -0300

    [#39] - Agrega schema event

---
+++ src/api/schemas/event.ts
@@ -0,0 +1,22 @@
+import { boolean, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { user } from './user'
+
+
+export const event = pgTable('event', {
+    id: serial('id').primaryKey(),
+    name: text('name'),
+    description: text('description'),
+    startDate: timestamp('start_date'),
+    endDate: timestamp('end_date'),
+    altOrganizer: text('alt_organizer'),
+    imageUrl: text('imageUrl'),
+    peopleLimit: integer('people_limit'),
+    venueName: text('venue_name'),
+    venueAddress: text('venue_address'),
+    // Audit fields
+    createdBy: serial('created_by').references(() => user.id),
+    createdAt: timestamp('created_at'),
+    updatedAt: timestamp('updated_at'),
+    enabled: boolean('enabled'),
+    deleted: boolean('deleted'),
+})
