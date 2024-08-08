4db14d4c67d22dcd83382d150fd446a47f7f3078
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-08-08 16:00:05 -0300

    [#39] - Implementa EventService

--- src/app/providers/event.service.ts
+++ src/app/providers/event.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject, Observable, tap } from 'rxjs'
+import { Event } from '../interfaces/event.model'
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+  selectedEvent$ = new BehaviorSubject<Event | null>(null);
 
-  constructor() { }
+  http = inject(HttpClient);
+
+  getById(id: number): Observable<Event> {
+    return this.http.get<Event>(`${environment.redirectUri}/api/event/${id}`);
+  }
+
+  // Asigna un evento por defecto para la aplicación
+  // TODO: Eliminar al implementar la selección de evento
+  loadDefault(): Observable<Event> {
+    return this.getById(environment.selectedEventId).pipe(
+        tap(event => this.selectedEvent$.next(event)),
+    )
+  }
 }
