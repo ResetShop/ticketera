0d3ca34872acd850d9ae9255408543c4c65e4190
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-08-08 16:05:28 -0300

    [#39] - Corrige test

--- src/app/app.resolver.spec.ts
+++ src/app/app.resolver.spec.ts
@@ -3,13 +3,18 @@ import { ResolveFn } from '@angular/router';
 
 import { appResolver } from './app.resolver';
 import { Event } from './interfaces/event.model'
+import { EventService } from './providers/event.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('appResolver', () => {
   const executeResolver: ResolveFn<Event> = (...resolverParameters) =>
       TestBed.runInInjectionContext(() => appResolver(...resolverParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [EventService],
+    });
   });
 
   it('should be created', () => {
