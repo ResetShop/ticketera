55939a63d258c4e154b927714149031cc6dcdf22
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-10-15 16:17:13 -0300

    [#39] - Elimina corrección de timezone en cliente

--- src/app/pages/ticket-detail/ticket-detail.component.ts
+++ src/app/pages/ticket-detail/ticket-detail.component.ts
@@ -24,8 +24,8 @@ import { Ticket } from '../../interfaces/ticket.model';
 				</div>
 				<div class="mt-5 grid grid-cols-2">
 					<div class="text-left">
-						{{selectedEvent$.value?.startDate | date: 'dd/MM/yyyy' : 'GMT-3' }} <br />
-						{{selectedEvent$.value?.startDate | date: 'h:mm a' : 'GMT-3' }}
+						{{selectedEvent$.value?.startDate | date: 'dd/MM/yyyy' }} <br />
+						{{selectedEvent$.value?.startDate | date: 'h:mm a' }}
 					</div>
 					<div class="text-right"><span class="font-bold">{{selectedEvent$.value?.venueName}}</span><br />{{selectedEvent$.value?.venueAddress}}</div>
 				</div>
